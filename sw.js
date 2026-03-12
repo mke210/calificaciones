0f8cfc440116873568b7b9b2fbe31e84e4aad9b4
mandrake210 <elprofechan@gmail.com>
2026-03-11 20:55:34 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+// Service Worker para Registro de Calificaciones
+const CACHE = 'regcalif-v1';
+const ASSETS = ['/calificaciones/', '/calificaciones/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(()=>{})));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => 
+    Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/calificaciones/')))
+  );
+});
